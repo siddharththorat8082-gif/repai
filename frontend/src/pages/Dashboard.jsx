2231ede9204a60901b7d2ed8b2057b26a6001513
hp <siddharththorat8082@gmail.com>
2026-04-26 18:15:36 +0530

    add payment button

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-function Dashboard() {
+const Dashboard = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.clear();
+    navigate('/login');
+  };
+const handlePayment = async () => {
+  const options = {
+    key: rzp_test_Si7pgj8Su2KnKF,
+    amount: 29900,
+    currency: "INR",
+    name: "RepAI",
+    description: "Premium Subscription",
+    handler: function (response) {
+      alert("Payment Successful 🎉");
+      console.log(response);
+    },
+    prefill: {
+      email: "user@example.com"
+    },
+    theme: {
+      color: "#FF3B30"
+    }
+  };
+
+  const rzp = new window.Razorpay(options);
+  rzp.open();
+};
+
   return (
     <div>
       <h1>Dashboard Page</h1>
+      <button onClick={handleLogout}>Logout</button>
+      <button onClick={handlePayment}>
+       Buy Premium ₹299
+    </button>
     </div>
   );
-}
+};
 
 export default Dashboard;
